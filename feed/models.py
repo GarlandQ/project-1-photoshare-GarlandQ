c2966b272e744064ec592e911c926eec736e414e
GarlandQ <garqiu@gmail.com>
2021-03-07 05:03:15 -0500

    got pics working, finishing up profile

--- feed/models.py
+++ feed/models.py
@@ -8,16 +8,17 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField()
-    pfp = models.ImageField(default='default.jpg', upload_to='')
+    pfp = models.ImageField(default="default.jpg", upload_to="profile_pictures")
 
     def __str__(self):
-        return f'{self.user.username} Profile'
+        return f"{self.user.username} Profile"
 
 
 # Users can have many posts (one to many relationship)
 class Posts(models.Model):
     postImage = models.ImageField(
-        upload_to=None, height_field=None, width_field=None, max_length=100)
+        upload_to=None, height_field=None, width_field=None, max_length=100
+    )
     postDescription = models.TextField()
     # If user is deleted, just delete the post also
     postUser = models.ForeignKey(User, on_delete=models.CASCADE)
