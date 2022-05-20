09dca6869db258388e52f222d63dba031290fd11
GarlandQ <garqiu@gmail.com>
2022-05-19 22:46:05 -0400

    heroku deploy

--- photoshare/settings.py
+++ photoshare/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "38nh!#9l=$+k$i6dqz$44js+b(osw*9#!y6*z$tgelfy3wj844"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -137,3 +138,7 @@ LOGIN_URL = "/login/"
 
 # url to be redirected to if you are logged in
 LOGIN_REDIRECT_URL = "/"
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+django_heroku.settings(locals())
